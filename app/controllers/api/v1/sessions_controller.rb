cd2cee14853fa119219f1e1f18053ad649af0829
Peter <ppapadim12@gmail.com>
2018-01-02 19:36:44 -0500

    backend test deploy

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
-  
-  skip_before_action :authenticate
-  
+
+  # skip_before_action :authenticate
+
   def create
     user = User.find_by(email: auth_params[:email])
     if user.authenticate(auth_params[:password])
@@ -9,10 +9,10 @@ class Api::V1::SessionsController < ApplicationController
       render json: {jwt: jwt}
     end
   end
-  
+
   private
     def auth_params
       params.require(:auth).permit(:email, :password)
     end
-    
+
 end
