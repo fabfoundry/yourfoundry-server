39ab7f968c452a2759f78ecc99c6e76a97461da1
Peter <ppapadim12@gmail.com>
2018-01-17 21:27:43 -0500

    created project and user_projects models and modified schema. index and create actions created for projects.

---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::ProjectsController < ApplicationController
+
+
+  def index
+    render json: {projects: @current_user.projects}
+  end
+
+
+  def create
+    exists = @current_user.projects.select do |project|
+      project[:name].downcase == project_params[:newProject].downcase
+    end
+
+    if exists.empty?
+      @current_user.projects << Project.create(name: project_params[:newProject])
+      render json: {request: "complete", projects: @current_user.projects}
+    else
+      render json: {request: "exists"}
+    end
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:newProject)
+    end
+
+end
